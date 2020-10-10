e527cb25c6d0834e133ed403315dfbf021fba6c2
edmundo8avgz <67813513+edmundo8avgz@users.noreply.github.com>
2020-10-10 14:48:05 -0500

    subirRstudio
    prueba subir archivo de git a r studio

---
+++ Ejemplo01_Funciones.R
@@ -0,0 +1,34 @@
+function_name <- function(arg1, arg2)
+{
+  #	Manipular argumentos arg1 y arg2 de cierta forma y regresar un valor
+}
+
+boring_function <- function(x) {
+  x
+}
+
+boring_function
+
+
+identidad  <- function(X){
+X  
+}
+
+identidad
+identidad(5)
+identidad("Hola")
+
+#arreglo es llamada tambien vector
+
+identidad(c(1,2,3))
+
+#Funcion Promedio
+
+promedio <- function(calificaciones) {
+  sum(calificaciones)/length(calificaciones)  
+}
+
+#Poner en practiva funcion Ejecutar
+promedio(c(10,9,8,7,6,8,7,6,8,8,7,7,8,6,6))
+
+
